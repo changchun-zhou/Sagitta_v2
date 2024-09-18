03232be9bcabc833fa5f4761a383660a187a6756
changchun-zhou <2835079913@qq.com>
2024-09-18 23:19:27 +0800

    style(src/): rm test.v

--- hardware/src/test.v
+++
@@ -1,8 +0,0 @@
-DFQM2DM \PSUM[7].datain_reg[30]  (.CK(CTS_1287),
-        .D(n_40713),
-        .Q(\PSUM[7].datain [30]));
-
-DFQRM2DM \PSUM[7].datain_reg[30]  (.CK(CTS_1287),
-        .D(n_40713),
-        .Q(\PSUM[7].datain [30]),
-        .RB(rst_n));
